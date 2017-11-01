3f86c1863767f6a9a6bb09845e2e23ed902d0b7b
Oleg Chornyy <scoop7@ukr.net>
2017-11-02 00:34:56 +0200

    HW2

---
+++ GeekHub-JS2017 HW2/js/main.js
@@ -0,0 +1,189 @@
+//FIRST THING FIRST
+
+//DESCRIPTORS
+
+
+
+function Body(figure) {
+    Object.defineProperty(this, "Figure", {
+        value: figure,
+        writable: false,
+        enumerable: true,
+        configurable: false
+    });
+    Object.defineProperty(this, "Mole", {
+        value: false,
+        enumerable: false
+    });
+ 
+    function isSkinColor(color) {
+        return color in ['black', 'white', 'yellow', 'brown'];
+    }
+    var skinColor;
+    Object.defineProperty(this, "SkinColor", {
+        enumerable: false,
+        get: function() {
+            return skinColor;
+        },
+        set: function(val) {
+            if (!isSkinColor(val)) {
+                return;
+            }
+            skinColor = val;
+        }
+    });
+    Object.defineProperty(this, "Waist", {
+        enumerable: true,
+        value: function() {
+            alert("Thin and sexy.")
+        }
+    });
+}
+ 
+var herBody = new Body("Feminine");
+herBody.Figure = "Slim";
+herBody.SkinColor = "brown";
+herBody.Mole = "On chick!";
+herBody.Waist(); // alerts "Thin and sexy."
+  for (var name in herBody)
+    alert(name + ": " + herBody[name]); // alerts "Figure" and "Waist", but not SkinColor
+
+
+
+
+// SECOND THING SECOND
+
+
+
+// The constructor to decorate
+
+function ZazVida() { 
+
+  this.cost = function() { return 253050; }; 
+  this.bodyType = function() { return "sedan"; }; 
+
+} 
+
+// 2015 year model Decorator 
+function lowerPrice(zazVida) { 
+
+  var v = zazVida.cost(); 
+  zazVida.cost = function() { 
+    return v - 24100; 
+  }; 
+
+} 
+
+// Lux Decorator 
+function Lux(zazVida){
+
+  var v = zazVida.cost(); 
+  zazVida.cost = function(){
+    return  v + 21690;
+  };
+
+}
+ 
+// 2015 year Lux Decorator
+function LowerLux(zazVida){
+
+  var v = zazVida.cost(); 
+  zazVida.cost = function(){
+     return  v + 2410;
+  };
+
+}
+
+var zv = new ZazVida(); 
+lowerPrice(zv); 
+Lux(zv);
+LowerLux(zv);
+
+// Outputs: 253050
+console.log(zv.cost());
+
+// Outputs: sedan
+console.log(zv.bodyType());
+
+
+
+
+
+
+
+//LAST THING LAST
+
+
+
+// toString
+
+
+function people(city, population) {
+	return {
+		city: city,
+		population: population,
+		
+		toString: function () {
+			return "[People " + "\"" + city + "\" " + population +  "]";
+		}
+	}
+}
+
+var cherkasyPopulation = people("CHERKASY", 283e3);
+alert(cherkasyPopulation); // [cherkasyPopulation "Cherkasy" 283000]
+
+
+
+// valueOf
+
+function people(city, population) {
+	return {
+		city: city,
+		population: population,
+		
+		toString: function () {
+			return "[People " + 
+				"\"" + city + "\" " +
+				population +
+			"]";
+		},
+		
+		valueOf: function () {
+			return population;
+		}
+	};
+}
+
+var cherkasyPopulation = people("Cherkasy", 283e3);
+var kyivPopulation = people("Kyiv",  280e4);
+var kharkivPopulation = people("Kharkiv", 143e4);
+
+alert(cherkasyPopulation); // [cherkasyPopulation "Cherkasy" 283000
+
+var threeCitiesPopulation = cherkasyPopulation + kyivPopulation + kharkivPopulation;
+
+alert(threeCitiesPopulation); // 4513000
+
+
+// JSON.stringify
+
+var cityName = {"city":"Cherkasy", "population":283000};
+JSON.stringify(cityName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
